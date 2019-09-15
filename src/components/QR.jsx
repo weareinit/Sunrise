70fa4f1a79a785f4e0bf643a17ff521cb0ffe503
Christopher Rodriguez <mrmcpowned@users.noreply.github.com>
2019-09-15 18:57:23 -0400

    Added notifications and data for check in

--- src/components/QR.jsx
+++ src/components/QR.jsx
@@ -2,56 +2,101 @@ import React from 'reactn';
 import QrReader from 'react-qr-reader'
 import axios from 'axios';
 import { modalTypes } from './ModalRoot';
+import { store } from 'react-notifications-component';
 
-class QR extends React.Component
-{
-    handleScan = data => 
-    {
+class QR extends React.Component {
+
+    hackerCheckIn = () => {
+
+    }
+
+    generalCheckIn = () => {
+        
+    }
+
+    handleScan = data => {
 
         /* We shouldn't read QR codes while the user is in the process of
         checking people in/redeeming items
          */
-        if(data != null && !this.global.codeScanned)
-        {
+        if (data != null && !this.global.codeScanned) {
             this.setGlobal({ codeScanned: true })
 
             // The auth QR code is Base64 encoded, which is how we know 
             // it's the auth QR code
             let isBase64 = false;
             let authJson;
-            try{
+            try {
                 authJson = JSON.parse(atob(data));
 
                 isBase64 = true;
-            } catch(e)
-            {
+            } catch (e) {
                 console.log(e);
             }
 
             //Store the auth token in the state and localStorage
-            if(isBase64){
-                try
-                {
-                    if(typeof authJson.password == "undefined")
+            if (isBase64) {
+                try {
+                    if (typeof authJson.password == "undefined")
                         throw new Error("Auth json is incorrect.");
-                    
-                    axios.post("https://api.shellhacks.net/token",authJson)
+
+                    store.addNotification({
+                        title: "Authenticating...",
+                        message: "Authenticating with API",
+                        type: "info",
+                        insert: "bottom",
+                        container: "bottom-center",
+                        animationIn: ["animated", "fadeIn"],
+                        animationOut: ["animated", "fadeOut"],
+                        dismiss: {
+                            duration: 5000,
+                            onScreen: true
+                        }
+                    });
+
+                    axios.post("https://api.shellhacks.net/token", authJson)
                         .then(res => {
                             const authToken = res.data.data;
                             this.setGlobal({ authToken });
                             window.localStorage.setItem("authToken", JSON.stringify(authToken));
+                            store.addNotification({
+                                title: "Authenticated",
+                                message: "Successfully authenticated with API!",
+                                type: "success",
+                                insert: "bottom",
+                                container: "bottom-center",
+                                animationIn: ["animated", "fadeIn"],
+                                animationOut: ["animated", "fadeOut"],
+                                dismiss: {
+                                    duration: 5000,
+                                    onScreen: true
+                                }
+                            });
                         }).finally(() => {
                             setTimeout(() => {
-                                this.setGlobal({codeScanned: false})
+                                this.setGlobal({ codeScanned: false })
                             }, 1000)
+                        }).catch(err => {
+                            store.addNotification({
+                                title: "Error",
+                                message: `Authentication failed: ${err}`,
+                                type: "danger",
+                                insert: "bottom",
+                                container: "bottom-center",
+                                animationIn: ["animated", "fadeIn"],
+                                animationOut: ["animated", "fadeOut"],
+                                dismiss: {
+                                    duration: 5000,
+                                    onScreen: true
+                                }
+                            });
                         })
-                    
+
                 }
-                catch(e)
-                {
+                catch (e) {
                     console.log(e);
                 }
-            // Or check if the shell ID exists
+                // Or check if the shell ID exists
             } else {
                 this.setGlobal({ shellID: data });
                 const { shellID, authToken } = this.global;
@@ -63,31 +108,30 @@ class QR extends React.Component
                         }
                     }
                 )
-                .then((res) => {
-                    const { data } = res.data;
-                    
-                    if(data.checkIn){
-                        throw new Error("User is already checked in.")
-                    }
-                    
-                    this.setGlobal({user: data, currentModal: modalTypes.USER_INFO})
-                })
-                .catch((e) => {
-                    console.log(e)
-                })
-                .finally(() => {
-                    setTimeout(() => {
-                        this.setGlobal({codeScanned: false})
-                    }, 1000)
-                })
+                    .then((res) => {
+                        const { data } = res.data;
+
+                        if (data.checkIn) {
+                            throw new Error("User is already checked in.")
+                        }
+
+                        this.setGlobal({ user: data, currentModal: modalTypes.USER_INFO })
+                    })
+                    .catch((e) => {
+                        console.log(e)
+                    })
+                    .finally(() => {
+                        setTimeout(() => {
+                            this.setGlobal({ codeScanned: false })
+                        }, 1000)
+                    })
             }
         }
     }
 
-    render()
-    {
+    render() {
         return (
-            <QrReader 
+            <QrReader
                 showViewFinder={true}
                 facingMode="environment"
                 className="reader"
